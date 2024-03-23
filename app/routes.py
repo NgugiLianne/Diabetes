2ec92c4e59d283f311037f99ed010d083624397b
ngugilianne <ngugilianne@gmail.com>
2024-03-23 18:25:45 +0300

    Initial commit

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ import joblib
 app = Flask(__name__)
 
 # Load the trained model
-model = joblib.load('models/trained_model.pkl')
+model = joblib.load('model/Analysis.ipynb') 
 
 @app.route('/')
 def index():
